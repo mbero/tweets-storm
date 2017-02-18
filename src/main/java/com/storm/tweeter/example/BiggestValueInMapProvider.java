0a95fb2f769b023800dd3efd23839d6cac20f5a0
mbero <mberendt546@gmail.com>
2017-02-19 00:18:09 +0100

    Added getGivenAmountOfBiggestValuesWithKeysAsStringFromGivenHashmap

--- src/main/java/com/storm/tweeter/example/BiggestValueInMapProvider.java
+++ src/main/java/com/storm/tweeter/example/BiggestValueInMapProvider.java
@@ -1,7 +1,10 @@
 package com.storm.tweeter.example;
 
+import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Iterator;
 import java.util.LinkedHashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 import java.util.Set;
@@ -15,6 +18,30 @@ public class BiggestValueInMapProvider {
 		String firstKeyString = firstKey.get();
 		Integer biggestIntegerValue = sortedMap.get(firstKeyString);
 		return biggestIntegerValue;
+	}
+
+	public static String[] getGivenAmountOfBiggestValuesWithKeysAsStringFromGivenHashmap(Map<String, Integer> counts,
+			int numberOfBiggestValues) {
+		String[] arrayToReturn = new String[numberOfBiggestValues];
+		List<String> givenAmountOfValuesWithKeys = new ArrayList<String>();
+		Map<String, Integer> sortedMap = sortByValue(counts);
+		Set<String> keysetFromSortedMap = sortedMap.keySet();
+		Iterator<String> keysetIterator = keysetFromSortedMap.stream().iterator();
+
+		while (keysetIterator.hasNext()) {
+			if (numberOfBiggestValues > 0) {
+				String currentKey = keysetIterator.next();
+				String currentValueWithKey = "word " + currentKey + " occurs : " + sortedMap.get(currentKey) + " times";
+				givenAmountOfValuesWithKeys.add(currentValueWithKey);
+				numberOfBiggestValues -= 1;
+			} else {
+				break;
+			}
+		}
+		for (int i = 0; i < givenAmountOfValuesWithKeys.size(); i++) {
+			arrayToReturn[i] = givenAmountOfValuesWithKeys.get(i);
+		}
+		return arrayToReturn;
 	}
 
 	public static <K, V extends Comparable<? super V>> Map<K, V> sortByValue(Map<K, V> map) {
